b1fe007dce26b00dbb5ab29afa9f2b636c52909a
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2016-07-20 18:02:48 -0500

    create three links files at once

--- circos/interactions2circoslinks.py
+++ circos/interactions2circoslinks.py
@@ -5,19 +5,22 @@ import csv
 chroms = [str(c) for c in range(1,24)] + ['X','Y']
 # red gene loci
 loci = {}
-csvr = csv.reader( open('data/genes_loci.tsv'),  delimiter='\t')
+csvr = csv.reader( open('../data/genes_loci.tsv'),  delimiter='\t')
 csvr.next()
 for row in csvr:
     (chrom, start, end, symbol) = row
 
     if chrom in chroms:
-        loci[symbol] = (chrom,start,end)
+        loci[symbol] = ("hs%s" % chrom, start, end)
 
 
 
-csvr = csv.reader(open("data/l1.csv"))
-
-for interaction in csvr:
-    (s,t) = interaction
-    if s in loci and t in loci:
-        print "hs"+" ".join(loci[s]),"hs"+" ".join(loci[t])
+for level in [1,2,3]:
+    with open("../fantom_tables/l%s.csv" % level) as l1, \
+         open("data/l%s_links.tsv" % level, 'w') as links:
+        csvr = csv.reader(l1)
+        csvw = csv.writer(links, delimiter=" ")
+        for interaction in csvr:
+            (s,t,w) = interaction
+            if s in loci and t in loci:
+                csvw.writerow( loci[s] + loci[t])
